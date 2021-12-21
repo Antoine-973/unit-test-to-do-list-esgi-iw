c158e4ba2141cc76ec105285b2db7b14d8eeba7c
alexbaudry0 <alexbaudry0@gmail.com>
2021-12-21 21:39:53 +0100

    Fonction User
    creation de l'utilisateur
    /user/new
      méthod : POST
      body :
        lastname
        firstname
        password
        email
        birthdate
    je vérifie que l'user n'existe pas déjà (email déjà utiliser) puis que tous les champs sont remplis et je crées le user.
    
    get user by id
      méthod : GET
      body : none
    /user/{user_id}
    
    return le user sous forme de JSON s'il existe sinon error 400
    
    get user todo list
    /user/{user_id}/todo-list
    return todolist if exist
    si le user exist pas > 400
    si la todo list exist pas > 400

--- src/Controller/ToDoListController.php
+++ src/Controller/ToDoListController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -18,9 +19,13 @@ class ToDoListController extends AbstractController
         ]);
     }
 
-    #[Route('/user/{user_id}/todo-list', name: 'todo-list')]
-    public function getUserTodoList(User $user)
+    #[Route('/user/{user_id}/todo-list', name: 'todo-list', methods:['GET'])]
+    public function getUserTodoList(User $user): JsonResponse|Response
     {
-        var_dump( $user->getToDoList() );
+        if($user == null)
+            return new Response('User does not exist', Response::HTTP_BAD_REQUEST) ;
+        if($user->getToDoList() == null)
+            return new Response('Todo list is empty or do not exist', Response::HTTP_BAD_REQUEST) ;
+        return new JsonResponse(json_encode($user->getToDoList()),Response::HTTP_BAD_REQUEST) ;
     }
 }
