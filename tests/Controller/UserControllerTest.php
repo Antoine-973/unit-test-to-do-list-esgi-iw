4401d6d296cd3d9f1608055874a5c4339606cad8
Antoine Saunier <antoine.saunier.pro@gmail.com>
2021-12-29 16:46:12 +0100

    update phpunit config and UserControllerTest

--- tests/Controller/UserControllerTest.php
+++ tests/Controller/UserControllerTest.php
@@ -6,12 +6,17 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class UserControllerTest extends WebTestCase
 {
-    public function testSomething(): void
+    public function testUserCreation(): void
     {
         $client = static::createClient();
-        $crawler = $client->request('GET', '/');
+        $client->request('POST', '/user/new',array(),array(),array("CONTENT_TYPE"=> "application/json"),json_encode(array(
+            "firstname"=>"TestCreateAUser",
+            "lastname"=>"ByWebTestCase",
+            "password"=>"123soleil",
+            "email"=>"test@gmail.com",
+            "birthdate"=>"2000/10/05"
+        )));
 
         $this->assertResponseIsSuccessful();
-        $this->assertSelectorTextContains('h1', 'Hello World');
     }
 }
